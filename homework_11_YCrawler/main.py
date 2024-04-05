ced9ff882e2e71870a8283489aa87d09504259d5
VladislavSoren <vlad224vlgu@mail.com>
2024-04-05 18:00:29 +0300

    feature(homework_11): added parsing restart in the time interval and parse_arguments

--- homework_11_YCrawler/main.py
+++ homework_11_YCrawler/main.py
@@ -1,12 +1,9 @@
 import os
 import re
-import datetime
-import logging
 import argparse
 import asyncio
 import aiofiles
 import aiohttp
-import requests
 from bs4 import BeautifulSoup
 from pathlib import Path
 import logging
@@ -43,22 +40,6 @@ def get_comment_urls_list(html):
     urls_comment_list = [BASE_URL + x.get('href') for x in data[1:]]
     return urls_news_list, urls_comment_list
 
-
-# def find_href_in_comments(comments_pages):
-#     """Функция поиска ссылок в комментариях. Принимает единственный аргумент comments_pages.
-#     Ожидается, что это будет итерируемый объект (сейчас список), содержащий ссылки на комментарии.
-#     Результатом выполенния функции является сбор ссылок со страниц комментариев к новостям.
-#     Новые ссылоки добавляются к уже существующей глобальной переменной link_list, сама функция
-#     ничего не возвращает."""
-#     global link_list
-#     for page in comments_pages:
-#         response = requests.get(page)
-#         bs_page = BeautifulSoup(response.text, 'lxml')
-#         data = bs_page.find_all('div', class_='comment')
-#         result = [x.find('a').get('href') for x in data \
-#                   if x.find('a') and 'https' in x.find('a').get('href')]
-#         if result:
-#             link_list += result
 
 def get_url_main_and_urls_in_comments(comments_page):
     bs_page = BeautifulSoup(comments_page)
@@ -160,50 +141,61 @@ async def save_comment_page(url_main, text):
     async with aiofiles.open(file_name, "a") as file:
         await file.write(text)
 
-    # """Асинхронная функция загрузки содержимого страниц. Принимает в качестве аргументов два значения:
-    # link - ссылка на страницу (ожидается строка) и parser - объект содержащий аргументы при запуске скрипта.
-    # Функция ничего не возвращает, а лишь выполняет запись страниц в указанной папке."""
-    # global news_dict
-    # filename = re.sub(r"[?:*<>,; /\\]", r'', link)
-    # full_filename = os.path.join(parser.d, filename[:20])
-    # if not news_dict[link]:
-    #     async with aiofiles.open(f'{full_filename}.html', mode='wb+') as handle:
-    #         async with aiohttp.ClientSession() as session:
-    #             try:
-    #                 response = await session.request('GET', link)
-    #                 page = await response.read()
-    #                 await handle.write(page)
-    #                 news_dict[link] = True
-    #             except aiohttp.client_exceptions.InvalidURL as err:
-    #                 logger.info(f'Ссылка {link} сейчас недоступна.')
-    #
-    # else:
-    #     logger.info(f'Ссылка {link} уже была обработана ранее.')
+
+def parse_arguments():
+    """Функция парсинга аргументов. Принимается два аргумента:
+    -d - аргумент для указания папки для сохранения страниц,
+    -t - аргумент для указания количества секунд между запусками обкачки."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parse_dir', type=str, help='Директория для парсинга',
+                        default="downloads")
+    parser.add_argument('--parse_pause', type=int,
+                        help='Количество секунд между запусками обкачки новостного комбинатора, сек',
+                        default=10)
+    parser.add_argument('--timeout', type=int, help='Таймаут ответа, сек',
+                        default=10)
+    parser.add_argument('--max_concurrent', type=int, help='Максимальное кол-во одновременных запросов',
+                        default=3)
+    parser.add_argument('--retry_interval', type=int, help='Таймаут повторной попытки запроса, сек',
+                        default=2)
+    parser.add_argument('--max_retry_count', type=int, help='Максимальное кол-во повторных запросов',
+                        default=5)
+    return parser.parse_args()
 
 
 async def main():
-    timeout = 10  # Adjust the timeout value (in seconds) as per your requirement
-    max_concurrent = 3
-    retry_interval = 2
-    max_retry_count = 5
+    parser = parse_arguments()
 
-    html = await get_start_page_html()
-    urls_news_list, urls_comment_list = get_comment_urls_list(html)
+    if not os.path.isdir(f'./{parser.parse_dir}'):
+        os.mkdir(parser.d)
 
-    # urls_comment_list = urls_comment_list[:5]
+    parse_pause = parser.parse_pause
+    timeout = parser.timeout
+    max_concurrent = parser.max_concurrent
+    retry_interval = parser.retry_interval
+    max_retry_count = parser.max_retry_count
 
-    responses = await fetch_all(urls_comment_list, max_concurrent, timeout, retry_interval, max_retry_count)
+    while True:
+        html = await get_start_page_html()
+        urls_news_list, urls_comment_list = get_comment_urls_list(html)
 
-    # run for fetching links from comments
-    tasks = []
-    for response in responses:
-        url_main, urls_in_comments_list = get_url_main_and_urls_in_comments(response)
-        if url_main and urls_in_comments_list:
-            tasks.append(fetch_comment_links(url_main, urls_in_comments_list, max_concurrent, timeout, retry_interval,
-                                             max_retry_count))
+        # urls_comment_list = urls_comment_list[:5]
 
-    if tasks:
-        responses = await asyncio.gather(*tasks)
+        responses = await fetch_all(urls_comment_list, max_concurrent, timeout, retry_interval, max_retry_count)
+
+        # run for fetching links from comments
+        tasks = []
+        for response in responses:
+            url_main, urls_in_comments_list = get_url_main_and_urls_in_comments(response)
+            if url_main and urls_in_comments_list:
+                tasks.append(
+                    fetch_comment_links(url_main, urls_in_comments_list, max_concurrent, timeout, retry_interval,
+                                        max_retry_count))
+
+        if tasks:
+            responses = await asyncio.gather(*tasks)
+
+        await asyncio.sleep(parse_pause)
 
 
 if __name__ == "__main__":
